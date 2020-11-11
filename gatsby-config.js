f94a71d308ceea30771cd6583a89c9aad97a7e5f
Vitor Correa <vitorcorrea@binaryforge.io>
2020-11-11 20:53:10 +0000

    setup sanity plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,28 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+// change common JS module to (module.exports) to ES module
+// See package.json : "cross-env NODE_OPTIONS=\"-r esm\"
+// by default Gatsby will surface GATSBY_ANYNAME but for SANITY_ or others we need the package.
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default {
+  siteMetadata: {
+    title: `Slicks Slices`,
+    siteUrl: `https://gatsby.pizza`,
+    description: `The Best pizza place`,
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      // this is the name of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: '3s4vlwm5',
+        dataset: 'production',
+        // "HotReload" for saniaty in dev mode
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
--- src/pages/pizzas.js
+++ src/pages/pizzas.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function PizzasPage() {
   return (
     <>
-      <p>I'm the Pizzas page</p>
+      <p>I'm the slicks spage</p>
     </>
   );
 }
