81b3034aee855fe671e231ccbbb02091a017b7fd
Vitor Correa <vitorcorrea@binaryforge.io>
2020-11-18 06:50:11 +0000

    pizza grid

---
+++ src/components/ToppingsFilter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ToppingsFilter() {
+  // 1 - Get a list of all toppings
+  // 2 - Get a list of all the Pizzas with their toppings
+  // 3 - Count how many pizzas are in each topping
+  // 4 - Loop over the list of toppings and display the topping and the count of pizzas in that topping
+  return (
+    <div>
+      <p>Toppings</p>
+    </div>
+  );
+}
--- src/pages/pizzas.js
+++ src/pages/pizzas.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import PizzaList from '../components/PizzaList';
+import ToppingsFilter from '../components/ToppingsFilter';
 
 export default function PizzasPage({ data: { pizzas } }) {
   // console.log(props.data.pizzas);
   return (
     <>
+      <ToppingsFilter />
       <PizzaList pizzas={pizzas.nodes} />
     </>
   );
